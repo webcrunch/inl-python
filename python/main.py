9630bee618ec2c7c94d81669e53764be4831ec49
jarl Lindquist <jarllinduist@gmail.com>
2022-10-26 10:40:02 +0200

    event driven listeners for the buttons and drop downs

--- python/main.py
+++ python/main.py
@@ -4,17 +4,59 @@ if w == None:
 # setting jquery to j
 j = w.jQuery
 fetch = w.fetch
+button = j(".button")
+off = j('.off')
+on = j('on')
+color_choosed = None
+color_picker_value = j('.color_chooser')
+blink_button = j('.E_blink')
 
 
 def button_click(e):
     print("Hej")
-    aio.run(sendData())
+    aio.run(send_get_Data())
 
 
-j(".button").on('click', button_click)
+# async def button_send(status):
+#     api_url = "http://localhost/power/" + status
+#     response = await (await fetch(api_url)).json()
+#     w.console.log(response)
 
 
-async def sendData():
-    api_url = "https://jsonplaceholder.typicode.com/todos/1"
+def test(ev):
+    print(ev)
+    aio.run(sendData(j('.color_chooser option:selected').val()))
+
+
+async def blink_function(ev):
+    print("eere")
+    # aio.run()
+
+
+# on.on('click', button_send('on'))
+# off.on('click', button_send('off'))
+color_picker_value.on('change', test)
+# color_picker_value.change(function():
+#     print("Handler for .change() called.")
+button.on('click', button_click)
+blink_button.on('click', blink_function)
+
+
+async def send_get_Data(url):
+    api_url = url
     response = await (await fetch(api_url)).json()
+
+
+async def sendData(color):
+    print(color)
+    # api_url = "https://jsonplaceholder.typicode.com/todos/1"
+    # response = await (await fetch(api_url)).json()
+    response = await(await fetch("http://localhost:5000/color", {
+        method: 'POST',
+        headers:
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+
+        body: JSON.stringify({"color": color})
+    })).json()
     w.console.log(response)
