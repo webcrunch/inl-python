5ba38b517e49dec8362c446c0574e0cdc6f088ee
jarl Lindquist <jarllinduist@gmail.com>
2022-10-31 15:30:27 +0100

    change the function hirachy and remove some imports

--- python/main.py
+++ python/main.py
@@ -1,9 +1,15 @@
-from os import system
-from browser import window as w, aio
-from browser import document, ajax
+# from localStoragePy import localStoragePy
+from browser import window as w, document, ajax
 from network_brython import send_url, timestamp_to_iso, connect, send, close
 from dom_io import print as _print, input as _input
 from datetime import datetime
+from api_call import alf as call
+# localStorage = localStoragePy('your-app-namespace', 'text')
+# localStorage.setItem("user_name", "stig")
+# data = localStorage.getItem("user_name")
+# print(data)
+
+
 # from pyfirmata import Arduino, util
 # board = pyfirmata.Arduino('/ dev/ttyS4')
 # board = Arduino('COM5')
@@ -40,13 +46,23 @@ def color_display(e):
     # j("body").css("background-color", j('#head').val())
 
 
-def blink(e):
+def write_log(time, user, message):
+    time_string = timestamp_to_iso(time)
+    print('time:', time, 'user:', user, 'message:', message)
+    log_template.append(f'<li>{message}({time_string})</li>') if user is 'system' else log_template.append(
+        f'<li>User {user} has done {message} ({time_string})</li>')
+
+
+def blink_handling(e):
     send('pushed the blink button') if j(
         'input[name=gradient]:checked').length < 1 else send('pushed the blink button with gradient altered:' + j(".slider").val())
     # j("body").css("background-color", j('#head').val())
 
 
 def chatt_cb(time, user, message):
+    user = user.replace('\'', '')
+    message = message.replace('.', ' ')
+    message = message.replace('\'', '')
     time_string = timestamp_to_iso(time)
     chat_template.append(f'<li>{message}({time_string})</li>') if user is 'system' else chat_template.append(
         f'<li>{message}({time_string})</li>')
@@ -54,23 +70,16 @@ def chatt_cb(time, user, message):
 
 
 def action_cb(time, user, message):
-    print('time', time, 'user', user, 'message', message)
+    user = user.replace('\'', '')
+    message = message.replace('.', '')
+    message = message.replace('\'', '')
+    # print('time:', time, 'user:', user, 'message:', message)
     write_log(time, message)
-
-
-def write_log(time, user, message):
-    time_string = timestamp_to_iso(time)
-    log_template.append(f'<li>{message}({time_string})</li>') if user is 'system' else log_template.append(
-        f'<li>User {user} has done {message} ({time_string})</li>')
 
 
 def set_a_log_test(e):
     j('.disclosed').toggle(200)  # .css("display", "flex")
     #
-
-
-def button_click(e):
-    print("Hej")
 
 
 def post_command(url, data):
@@ -99,13 +108,13 @@ def handle_connection(e):
 
 
 def gradient_check(e):
-    j('.slidecontainer').css('visibility', 'visible') if j(
-        'input[name=gradient]:checked').length == 1 else j('.slidecontainer').css('visibility', 'hidden')
+    j('.slidecontainer').css('visibility', 'visible') if \
+        j('input[name=gradient]:checked').length else j('.slidecontainer').css('visibility', 'hidden')
 
 
 def main_connect(e):
-    name = 'connect_from_body_user'
-    room = 'connect_from_body'
+    name = 'muse'
+    room = 'sting'
     connect(room, name, action_cb)
 
 
@@ -113,7 +122,7 @@ init_button.on('click', main_connect)
 color_pick_button.on('click', color_display)
 chatt_button.on('click', set_a_log_test)
 enter_room.on('click', handle_connection)
-blink_button.on('click', blink)
+blink_button.on('click', blink_handling)
 gradient_display.on('click', gradient_check)
 
 
