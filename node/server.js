2b5f5e94698f55f717bea343bae4385fd0f569b8
jarl Lindquist <jarllinduist@gmail.com>
2022-11-02 15:05:46 +0100

    adding hosting of html/python file and endpoints only gets

--- node/server.js
+++ node/server.js
@@ -1,17 +1,53 @@
+import path from "path";
 import express from "express";
 import five from "johnny-five";
 const board = new five.Board({ port: "COM5" });
+const color_dict = {
+  black: "#ffffff",
+  white: "#000000",
+  green: "#ff00ff",
+  red: "#ffff00",
+  blue: "#00ffff",
+  purple: { R: 0, G: 1, B: 0 },
+  yellow: "#ff0000",
+  cyan: { R: 0, G: 0, B: 1 },
+};
+const colors = [
+  "red",
+  "green",
+  "blue",
+  "purple",
+  "yellow",
+  "cyan",
+  "white",
+  "black",
+];
 const app = express();
 app.use(express.json());
 const server = (rgb) => {
   // custom color function is done
-  app.post("/color", async (req, res) => {
-    if (req.body.color.length < 7)
-      res.status(404).json({ error: "Bad color insertion" });
+  // an check if we sent in hex parameters instead of color name
+  app.get("/color/:color", async (req, res) => {
+    if (req.params.color.startsWith("#")) {
+      if (req.params.color.length == 7) {
+        rgb.color(req.params.color); //3,6,5  cyan: 00ff00,  yellow: ff0000, green: ff00ff
+        res.status(200).json({ colorchanged: true });
+      } else res.status(404).json({ error: "Bad color insertion." });
+    } else if (!colors.includes(req.params.color))
+      res.status(404).json({ error: "not the right color." });
     else {
-      rgb.color(req.body.color);
+      rgb.color(color_dict[req.params.color]); //3,6,5  cyan: 00ff00,  yellow: ff0000, green: ff00ff
       res.status(200).json({ colorchanged: true });
     }
+
+    //     # 6,5 RED
+    // # 3,6 GREEN
+    // # 3,5 BLUE
+    // # 5 PURPLE
+    // # 6 YELLOW
+    // # 3 CYAN
+    // # 0,0,0 WHITE
+    // # 1,1,1 BLACK
   });
 
   app.get("/off", (req, res) => {
@@ -20,7 +56,8 @@ const server = (rgb) => {
   });
 
   app.get("/on", (req, res) => {
-    rgb.color("#000000");
+    // rgb.color("#000000");
+    rgb.off();
     res.status(200).json(true);
   });
 
@@ -33,9 +70,56 @@ const server = (rgb) => {
     res.json(true);
   });
 
-  app.get("/blink/:sequence?", async (req, res) => {
-    rgb.blink(req.params.sequence);
-    res.json(true);
+  app.get("/rainbow", (req, res) => {
+    let rainbow;
+    let lastColor = "ff0000";
+    let red = 255,
+      green = 0,
+      blue = 0,
+      s = 0;
+    clearInterval(rainbow);
+    rainbow = setInterval(function () {
+      switch (s) {
+        case 0:
+          red -= 1;
+          green += 1;
+          if (red == 0) {
+            s = 1;
+          }
+          break;
+        case 1:
+          green -= 1;
+          blue += 1;
+          if (green == 0) {
+            s = 2;
+          }
+          break;
+        case 2:
+          blue -= 1;
+          red += 1;
+          if (blue == 0) {
+            s = 0;
+          }
+          break;
+      }
+      let a = red + "," + green + "," + blue;
+      a = a.split(",");
+      let b = a.map(function (x) {
+        x = parseInt(x).toString(16);
+        return x.length == 1 ? "0" + x : x;
+      });
+
+      b = "" + b.join("");
+
+      rgb.color(b);
+      lastColor = b;
+    }, 10);
+
+    res.json({ status: true });
+  });
+
+  app.get("/blink/:sequence?/", async (req, res) => {
+    rgb.blink(req.params.sequence != undefined ? req.params.sequence : 1000);
   });
 
   //   app.get("/power/:status", async (req, res) => {
@@ -56,14 +140,18 @@ board.on("ready", () => {
   //   const rgb = new five.Led.RGB([6, 5, 3]);
   const rgb = new five.Led.RGB({
     pins: {
-      red: 6,
+      red: 3,
       green: 5,
-      blue: 3,
+      blue: 6,
     },
   });
 
   server(rgb);
 });
+
+let __dirname = path.resolve();
+let pythonPath = path.join(__dirname, "../python");
+app.use(express.static(pythonPath));
 
 app.listen(5000, () => {
   console.log("App is listening on port 5000");
